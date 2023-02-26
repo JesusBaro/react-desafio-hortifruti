0360edaab1eccbfdbc3d37db6f61de49a0bf5b59
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-02-26 02:25:55 -0300

    projeto finalizado

--- react-hortifruti/src/App.js
+++ react-hortifruti/src/App.js
@@ -1,20 +1,23 @@
+import React, { Component } from 'react';
+import Alface from "./files/img/alface.png";
+import beterraba from "./files/img/beterraba.png";
+import cenoura from "./files/img/cenoura.png";
+import cereja from "./files/img/cereja.png";
+import laranja from "./files/img/laranja.png";
+import limao from "./files/img/limao.png";
+import manga from "./files/img/manga.png";
+import tomate from "./files/img/tomate.png";
+import botaoMenus from "./files/vetor/MinusCircle.png";
+import botaoMais from "./files/vetor/PlusCircle.png";
+import shopCart from "./files/vetor/Shopcart.png";
 import "./files/style.css"
-import alface from "./files/img/alface.png"
-import beterraba from "./files/img/beterraba.png"
-import cenoura from "./files/img/cenoura.png"
-import cereja from "./files/img/cereja.png"
-import laranja from "./files/img/laranja.png"
-import limao from "./files/img/limao.png"
-import manga from "./files/img/manga.png"
-import tomate from "./files/img/tomate.png"
 
-import React, { Component } from 'react'
 class HortifruttiPage extends Component {
 
   state = {
-    Produtos: [
+    produtos: [
       {
-        imagem: `${alface}`,
+        imagem: `${Alface}`,
         titulo: "alface"
       },
       {
@@ -45,35 +48,61 @@ class HortifruttiPage extends Component {
         imagem: `${tomate}`,
         titulo: "alface"
       },
+    ],
+    botoes :
+        [
+       {
+          imagemBotao : `${botaoMenus}`,
+            tituloBotao: "Botão para remover um item ao carrinho."
+        
+      },
+      {
+        imagemBotao : `${botaoMais}`,
+          tituloBotao: "Botão para adicionar um item ao carrinho."
+      
+    },
+    {
+      imagemBotao : `${shopCart}`,
+        tituloBotao: "Carrinho de compras."
+    
+  }
     ]
   }
 
 
   render() {
     return (
-      <body>
-        <div class="Header">
+      <section>
+        <div className="Header">
           <h1>Hortifruti</h1>
           <h2>VnW</h2>
         </div>
 
-        <section class="Main">
-          <h2>Nossos Produtos</h2>
-
-
-          {this.state.Produtos.map((item) => (
-            <div id="FruitSection">
-              <img src="{item.imagem}" alt="{item.titulo}" />
-            </div>
-          ))} 
-  {/* <img src={this.state.Produtos[0].imagem} alt="{item.imagem}" /> */}
-
+          <h2 className='TittleProductContainer'>Nossos Produtos</h2>
+        <section className="ProductContainer">
+         
+          <div className="FruitSection">
+            {this.state.produtos.map((item) => (
+              <div className="FruitBox">
+                <img className='imagensDosLegumes' src={item.imagem} alt={item.titulo} />
+              </div>
+            ))}
+          </div>
 
           <div id="ShopCarSection">
+            <div className='CarrinhoBox'>
+            <img className='imagemCarrinho' src= {this.state.botoes[2].imagemBotao} alt={this.state.botoes[2].tituloBotao} />
 
-          </div> 
+              <p className='Paragraph'>Arraste o seu produto aqui para colocar no carrinho</p>
+            </div>
+            <div className='CarrinhoButton'>
+            <img className='imagemBotao' src= {this.state.botoes[1].imagemBotao} alt={this.state.botoes[1].tituloBotao} />
+            <img className='imagemBotao' src= {this.state.botoes[0].imagemBotao} alt={this.state.botoes[0].tituloBotao} />
+            </div>
+
+          </div>
         </section>
-      </body>
+      </section>
 
     )
   }
